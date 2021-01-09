e91011af65e768ad554e5bc1d062e2b7bfc03a8c
Rajas1312 <you@example.com>
2021-01-09 16:40:37 +0530

    Added:code to find winnng or loosing condition

--- uitility.js
+++ uitility.js
@@ -5,7 +5,16 @@ let win=1;
 let lose=1
 class Gambler{
     getGame(){
-        
+        let result=Math.ceil(Math.random()*10%2)
+        if(result==1){
+            console.log("won the game")
+            stake=stake+win
+            
+        }
+        if(result==2){
+            console.log("lost the game")
+            stake=stake-lose
+        }
     }
 }
 module.exports=new Gambler();
