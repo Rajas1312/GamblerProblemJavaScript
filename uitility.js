99a094a2b72908cafc2851418a8ff898fac49271
Rajas1312 <you@example.com>
2021-01-09 16:19:42 +0530

    Added:valiables intialization

--- uitility.js
+++ uitility.js
@@ -1,7 +1,11 @@
 console.log("welcome to gambler problem")
+const INITIAL_STAKE=100
+let stake=100;
+let win=1;
+let lose=1
 class Gambler{
     getGame(){
-        console.log("hi")
+        
     }
 }
 module.exports=new Gambler();
